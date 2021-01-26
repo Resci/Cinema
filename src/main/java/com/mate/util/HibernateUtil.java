d70c0ececfe51a5eaa1eb866cfda7f78a0559e18
Resci <vIsUAl123>
2021-01-26 18:28:49 +0200

    Add private constructor to HibernateUtil

--- src/main/java/com/mate/util/HibernateUtil.java
+++ src/main/java/com/mate/util/HibernateUtil.java
@@ -7,6 +7,9 @@ import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
     private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private HibernateUtil() {
+    }
       
     private static SessionFactory buildSessionFactory() {
         try {
