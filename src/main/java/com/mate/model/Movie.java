36f53375c3cdebf54c2eba30f8fc5b0fdce38e8b
Resci <vIsUAl123>
2021-01-26 18:25:47 +0200

    Change method order in Movie

--- src/main/java/com/mate/model/Movie.java
+++ src/main/java/com/mate/model/Movie.java
@@ -14,6 +14,30 @@ public class Movie {
     private String title;
     private String description;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     @Override
     public String toString() {
         return "Movie{"
@@ -40,29 +64,5 @@ public class Movie {
     @Override
     public int hashCode() {
         return Objects.hash(id, title, description);
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 }
